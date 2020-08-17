f00e6ad4fc9e3cf8947ed94e91e80a983ecd6482
ougfh <argali96@gmail.com>
2020-08-17 16:34:49 +0300

    Add summary and daily icons report tests

--- test/forecasts/core_test.clj
+++ test/forecasts/core_test.clj
@@ -1,7 +1,25 @@
 (ns forecasts.core-test
   (:require [forecasts.core :as sut]
-            [clojure.test :refer :all]))
+            [clojure.test :refer [are is testing deftest]]
+            [clojure.string :as cstr]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def forecast-example
+  {:currently {:summary            "Drizzle"
+               :precip-probability 0.9}
+   :hourly    {:summary "Rain."}
+   :daily     {:data [{:icon "rain"}
+                      {:icon "rain"}
+                      {:icon "partly-cloudy-day"}]}})
+
+(deftest summary-report
+  (let [report (sut/get-summary-report forecast-example)]
+    (testing "target keys are in report"
+      (are [x y] (cstr/includes? x y)
+        report (get-in forecast-example [:currently :summary])
+        report (str (get-in forecast-example [:currently :precip-probability]))
+        report (get-in forecast-example [:hourly :summary])))))
+
+(deftest daily-icons-report
+  (let [report             (sut/get-daily-icons-report forecast-example)
+        expected-icons-str "2 days rain, 1 days partly-cloudy-day"]
+    (is (cstr/includes? report expected-icons-str))))
